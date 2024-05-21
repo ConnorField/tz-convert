806219655009fbece707067f0bde543301a6dda0
Connor Field <connor@connorfield.com>
2024-05-21 15:14:37 +0100

    fix: term output

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,62 @@
 use chrono::prelude::*;
 use chrono_tz::Tz;
+use prettytable::{Table, row, cell};
+use colored::*;
 use std::env;
 
 fn main() {
     let local_time = Local::now();
-    println!("Local time: {}", local_time);
 
     let timezones = vec![
-        chrono_tz::UTC,
-        chrono_tz::Europe::London,
-        chrono_tz::US::Eastern,
-        chrono_tz::US::Central,
-        chrono_tz::US::Mountain,
-        chrono_tz::US::Pacific,
-        chrono_tz::Europe::Berlin,
-        chrono_tz::Asia::Tokyo,
-        chrono_tz::Australia::Sydney,
+        ("UTC", chrono_tz::UTC),
+        ("US/Eastern", chrono_tz::US::Eastern),
+        ("US/Central", chrono_tz::US::Central),
+        ("US/Mountain", chrono_tz::US::Mountain),
+        ("US/Pacific", chrono_tz::US::Pacific),
+        ("Europe/London", chrono_tz::Europe::London),
+        ("Europe/Berlin", chrono_tz::Europe::Berlin),
+        ("Asia/Tokyo", chrono_tz::Asia::Tokyo),
+        ("Australia/Sydney", chrono_tz::Australia::Sydney),
     ];
 
-    for tz in timezones {
+    let mut table = Table::new();
+    table.add_row(row![
+        "Timezone".bold().underline().cyan().to_string(),
+        "Current Time".bold().underline().cyan().to_string()
+    ]);
+
+    table.add_row(row![
+        "Local time".bold().green().to_string(),
+        local_time.format("%Y-%m-%d %H:%M:%S %Z").to_string()
+    ]);
+
+    for (name, tz) in timezones {
         let tz_time = local_time.with_timezone(&tz);
-        println!("Time in {}: {}", tz, tz_time);
+        table.add_row(row![
+            name.bold().blue().to_string(),
+            tz_time.format("%Y-%m-%d %H:%M:%S %Z").to_string()
+        ]);
     }
+
+    table.printstd();
 
     if let Ok(tz_str) = env::var("TZ") {
         match tz_str.parse::<Tz>() {
             Ok(system_tz) => {
                 let system_time = local_time.with_timezone(&system_tz);
-                println!("System configured timezone ({}) time: {}", system_tz, system_time);
+                println!(
+                    "\n{}{}{}",
+                    "System configured timezone (".bold().yellow().to_string(),
+                    tz_str.bold().yellow().to_string(),
+                    format!(
+                        ") time: {}",
+                        system_time.format("%Y-%m-%d %H:%M:%S %Z")
+                    ).bold().yellow().to_string()
+                );
             },
-            Err(_) => println!("Failed to parse system timezone."),
+            Err(_) => println!("{}", "Failed to parse system timezone.".bold().red().to_string()),
         }
     } else {
-        println!("TZ environment variable is not set.");
+        println!("{}", "TZ environment variable is not set.".bold().red().to_string());
     }
 }
